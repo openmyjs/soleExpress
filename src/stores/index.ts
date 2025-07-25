261026ad79b20a9d361b4159ac1aacfc8f0f942a
瀚都软件 <xoyycc@163.com>
2025-07-25 10:15:36 +0800

    2025年7月25日10:15:33

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,46 @@
-const map: Map<string, any> = new Map()
+const map: Map<KeyType, ValueType> = new Map()
 
-export const stores = map
+export interface KeyType {
+  config:string
+}
+export interface ValueType {
+  config:{
+    /**  是否CPU集群配置 */
+    cluster: boolean
+    /** 服务配置 */
+    server:{
+      /**
+       * 端口
+       * */
+      port: number
+      /**
+       * 密钥
+       * */
+      tokenSecret: string
+      /**
+       * 密钥过期时间
+       * */
+      tokenExpire: {
+        /**
+         * 密钥过期时间
+         * */
+        expiresIn: string
+        /**
+         * 记住我秘钥过期时间
+         * */
+        expiresInMe: string
+      }
+    }
+    /** 数据库配置 */
+    db:{
+      mongo:{
+        uri: string
+        options: any
+      }
+    }
+    /** CORS 配置 */
+    cors: string[]
+  }
+}
+
+export const stores:Map<KeyType, ValueType> = map
